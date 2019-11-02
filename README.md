5194f70e5d81341b582c9a57986ca9393a12d8bd
Chris <csalois@gmail.com>
2019-11-02 11:58:31 -0400

    fix file names and add README to follow convention

---
+++ README.md
@@ -0,0 +1,11 @@
+Caesar Cipher
+
+Caesar Cipher encrypts a MESSAGE by shifting a letter up or down the 
+alphabet by a KEY amount. If the KEY were 3 and the MESSAGE, “Hello!”.
+The Caesar Cipher will shift each letter up the alphabet 3 spaces, 
+leaving punctuation and spaces untouched but keeping capital letters 
+capitalized. If a letter is shifted past the end of the alphabet it will
+loop back around, “Z” with a KEY of 1 becomes “A” and “a” with a KEY of
+-1 becomes “z”. caesar_cipher will accept any positive or negative KEY.
+
+This is my first project learning sinatra.
--- main.rb
+++ main.rb
@@ -1,13 +1,3 @@
-# Caesar Cipher
-
-# Caesar Cipher encrypts a MESSAGE by shifting a letter up or down the 
-# alphabet by a KEY amount. If the KEY were 3 and the MESSAGE, “Hello!”.
-# The Caesar Cipher will shift each letter up the alphabet 3 spaces, 
-# leaving punctuation and spaces untouched but keeping capital letters 
-# capitalized. If a letter is shifted past the end of the alphabet it will
-# loop back around, “Z” with a KEY of 1 becomes “A” and “a” with a KEY of
-# -1 becomes “z”. caesar_cipher will accept any positive or negative KEY.
-
 require 'sinatra'
 require 'sinatra/reloader'
 
